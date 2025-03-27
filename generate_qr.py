9bb5765a1eb1946b63a60292c6e8407c67bcf4ea
alvikhanom <alvikhanom2007@gmail.com>
2025-03-27 06:09:21 +0000

    generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -2,11 +2,9 @@ import qrcode
 
 website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
-qr = qrcode.QRCode(version=1, box_size=5, border=5)
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data(website_link)
 qr.make()
 
-img = qr.make_image(fill_color='black', back_color='white')
-file_path = 'youtube_qr.png'
-img.save(file_path)
-
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+img.save('example_qr.png')
